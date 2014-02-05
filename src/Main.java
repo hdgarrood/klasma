1c1453140f970b3bfce78bdcaefaedb69b0de4dd
Harry Garrood <hdgarrood@gmail.com>
2014-02-05 19:17:22 +0000

    Add NoteValue.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(NoteName.C.toInt());
+		System.out.println(NoteValue.Quaver.dotted().length());
 	}
 }
---
+++ src/NoteValue.java
@@ -0,0 +1,23 @@
+public class NoteValue {
+	public static NoteValue Semibreve      = new NoteValue(1.0);
+	public static NoteValue Minim          = new NoteValue(1.0/2);
+	public static NoteValue Crotchet       = new NoteValue(1.0/4);
+	public static NoteValue Quaver         = new NoteValue(1.0/8);
+	public static NoteValue Semiquaver     = new NoteValue(1.0/16);
+	public static NoteValue Demisemiquaver = new NoteValue(1.0/32);
+
+	// The ratio of this NoteValue's length to a semibreve's.
+	private double value;
+	
+	public NoteValue(double value) {
+		this.value = value;
+	}
+	
+	public double length() {
+		return this.value;
+	}
+	
+	public NoteValue dotted() {
+		return new NoteValue(this.value * 1.5);
+	}
+}
