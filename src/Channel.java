f2d494ab179ac8c08e9082d5afc2f867945ef457
Harry Garrood <hdgarrood@gmail.com>
2014-02-07 22:23:32 +0000

    Add incomplete Channel

---
+++ src/Channel.java
@@ -0,0 +1,20 @@
+public class Channel {
+    private Waveform waveform;
+    private Note[] notes;
+
+    public Channel(Waveform waveform, Note[] notes) {
+        if (waveform == null)
+            throw new IllegalArgumentException("waveform may not be null");
+
+        if (notes == null)
+            throw new IllegalArgumentException("notes may not be null");
+
+        this.waveform = waveform;
+        this.notes = notes;
+    }
+
+    public byte amplitudeAt(double t) {
+        // todo
+        return 0;
+    }
+}
