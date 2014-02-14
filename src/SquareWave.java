b3c3a42e23ac79e1704973a160261009fcf613e4
Harry Garrood <harry@garrood.me>
2014-02-14 21:07:28 +0000

    brevity

--- src/SquareWave.java
+++ src/SquareWave.java
@@ -2,7 +2,6 @@ public class SquareWave extends Waveform {
     public SquareWave() { }
 
     protected double _at(double t) {
-        if (t < 0.5) return 1;
-        return -1;
+        return t > 0.5 ? 1 : -1;
     }
 }
