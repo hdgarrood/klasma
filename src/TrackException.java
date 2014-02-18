985d5ab3b20ccd26d96d4f548b9c22e77a3f3485
Harry Garrood <harry@garrood.me>
2014-02-18 23:27:47 +0000

    Fix warning re serialVersionUID not existing

--- src/TrackException.java
+++ src/TrackException.java
@@ -1,4 +1,5 @@
 public class TrackException extends RuntimeException {
+    private static final long serialVersionUID = 1l;
     public TrackException(Throwable t) { super(t); }
     public TrackException(String msg) { super(msg); }
 }
