52d18a3831315fb3b476116d4ac0b3c423ad76e3
Harry Garrood <harry@garrood.me>
2014-03-28 18:12:54 +0000

    Fix SquareWave volume
    Now the RMS values of sawtooth, square, and triangle should be
    equivalent (0.577...) so that they all sound about the same volume

--- src/SquareWave.java
+++ src/SquareWave.java
@@ -1,7 +1,9 @@
 public class SquareWave extends Waveform {
     public SquareWave() { }
 
+    private double VOLUME = 0.577;
+
     protected double _at(double t) {
-        return t > 0.5 ? 1 : -1;
+        return VOLUME * (t > 0.5 ? 1 : -1);
     }
 }
